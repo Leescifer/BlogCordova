5dbec6b9af1c8d082a503bb69c1233cc718580b5
Leecifer <128757198+Leescifer@users.noreply.github.com>
2025-01-13 10:28:32 +0800

    Updates for routing and connecting to database

--- server/server.js
+++ server/server.js
@@ -1,22 +1,27 @@
-const express = ('express')
-const mysql = ('mysql')
-const cors = ('cors')
-const path = ('path')
+const express = require('express')
+const mysql = require('mysql')
+const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
 app.use(express.static(path.join(__dirname, "public")))
 app.use(cors())
-app.user(express.json())
+app.use(express.json())
+
 
 const port = 5000
 
 const db = mysql.createConnection({
-    
+
     host: "localhost",
     user: "root",
     password: "",
-    database: "students"
+    database: "cordova"
 
 }
 )
+
+app.listen(port, () => {
+    console.log('Naminaw ko ha! Diko bungol!')
+})
